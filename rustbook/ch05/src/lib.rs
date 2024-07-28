fc3830e34cf449c188be9aac3419a5efc707235a
takkyuuplayer <takkyuuplayer@gmail.com>
2024-07-28 21:45:24 +0900

    Add 5.1

---
+++ rustbook/ch05/src/lib.rs
@@ -0,0 +1,68 @@
+#[cfg(test)]
+mod tests {
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    #[test]
+    fn test_5_1() {
+        {
+            // basic
+            let mut user1 = User {
+                username: String::from("someusername123"),
+                email: String::from("someone@example.com"),
+                active: true,
+                sign_in_count: 1,
+            };
+
+            assert_eq!(user1.email, String::from("someone@example.com"));
+
+            user1.email = String::from("another@example.com");
+
+            assert_ne!(user1.email, String::from("someone@example.com"));
+        }
+        {
+            let user = build_user(String::from("some@example.com"), String::from("some"));
+            assert_eq!(user.email, String::from("some@example.com"));
+        }
+        {
+            // updates
+            let user = build_user(String::from("some@example.com"), String::from("some"));
+            assert_eq!(user.email, String::from("some@example.com"));
+            assert_eq!(user.username, String::from("some"));
+
+            let user2 = User {
+                email: String::from("a@example.com"),
+                username: String::from("Alice"),
+                ..user
+            };
+
+            assert_ne!(user.email, user2.email);
+
+            assert_eq!(user.active, user2.active);
+            assert_eq!(user.sign_in_count, user2.sign_in_count);
+        }
+        {
+            // tuple struct
+            let black = Color(0, 0, 0);
+            assert_eq!(black.0, 0);
+
+            let origin = Point(0, 0, 0);
+            assert_eq!(origin.0, 0);
+        }
+    }
+
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email,
+            username,
+            active: true,
+            sign_in_count: 1,
+        }
+    }
+}
