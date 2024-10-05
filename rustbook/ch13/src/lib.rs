4d987ac31619fbe9d1eb4775817c60058e62730d
takkyuuplayer <takkyuuplayer@gmail.com>
2024-10-05 16:56:26 +0900

    10.2

--- rustbook/ch13/src/lib.rs
+++ rustbook/ch13/src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+    use std::path::Iter;
+
     #[test]
     fn test_10_1() {
         {
@@ -45,8 +47,93 @@ mod tests {
 
             let equal_to_x = |z| z == x;
 
+            assert_eq!(4, x);
+
             let y = 4;
             assert!(equal_to_x(y));
+        }
+    }
+
+    #[test]
+    fn test_10_2() {
+        {
+            let v1 : Vec<i32> = vec![1, 2, 3];
+            let v2 : Vec<i32> = v1.iter().map(|x| x + 1).collect();
+            assert_eq!(v2, vec![2, 3, 4]);
+        }
+
+        {
+            // filtering
+
+            #[derive(PartialEq, Debug)]
+            struct Shoe {
+                size: u32,
+                style: String,
+            }
+
+            fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+                shoes.into_iter()
+                    .filter(|s| s.size == shoe_size)
+                    .collect()
+            }
+
+            let shoes = vec![
+                Shoe { size: 10, style: String::from("sneaker") },
+                Shoe { size: 13, style: String::from("sandal") },
+                Shoe { size: 10, style: String::from("boot") },
+            ];
+
+            let in_my_size = shoes_in_my_size(shoes, 10);
+
+            assert_eq!(
+                in_my_size,
+                vec![
+                    Shoe { size: 10, style: String::from("sneaker") },
+                    Shoe { size: 10, style: String::from("boot") },
+                ]
+            );
+        }
+
+        {
+            // own iterator
+            struct Counter {
+                count: u32,
+            }
+
+            impl Counter {
+                fn new() -> Counter {
+                    Counter { count: 0 }
+                }
+            }
+
+            impl Iterator for Counter {
+                type Item = u32;
+
+                fn next(&mut self) -> Option<Self::Item> {
+                    self.count += 1;
+
+                    if self.count < 6 {
+                        Some(self.count)
+                    } else {
+                        None
+                    }
+                }
+            }
+
+            {
+                let mut counter = Counter::new();
+
+                assert_eq!(counter.next(), Some(1));
+            }
+
+            {
+                let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+                    .map(|(a, b)| a*b)
+                    .filter(|x| x %3 == 0)
+                    .sum();
+
+                assert_eq!(18, sum);
+            }
         }
     }
 }
